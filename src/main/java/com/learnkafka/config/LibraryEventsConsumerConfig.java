ad61f26cf8b6bf90c1513a7110767f42b09e9ccf
Rakesh Nair <crucifyrakesh@gmail.com>
2026-06-07 17:50:20 +0530

    save operation

--- src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
+++ src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -1,9 +1,45 @@
 package com.learnkafka.config;
 
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.ssl.SslBundles;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.ContainerCustomizer;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.ContainerProperties;
 
 @Configuration
 @EnableKafka
 public class LibraryEventsConsumerConfig {
+
+    @Autowired
+    KafkaProperties kafkaProperties;
+
+    @Autowired
+    KafkaTemplate kafkaTemplate;
+
+//    @Autowired
+//    FailureService failureService;
+
+    @Bean
+    @ConditionalOnMissingBean(name = "kafkaListenerContainerFactory")
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, kafkaConsumerFactory
+                .getIfAvailable(() -> new DefaultKafkaConsumerFactory<>(this.kafkaProperties.buildConsumerProperties())));
+        factory.setConcurrency(3);
+        //factory.setCommonErrorHandler(errorHandler());
+        return factory;
+    }
 }
---
+++ src/main/java/com/learnkafka/service/LibraryEventsService.java
@@ -0,0 +1,47 @@
+package com.learnkafka.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.learnkafka.entity.LibraryEvent;
+import com.learnkafka.jpa.LibraryEventsRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class LibraryEventsService {
+
+    private LibraryEventsRepository libraryEventsRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    public LibraryEventsService(LibraryEventsRepository libraryEventsRepository) {
+        this.libraryEventsRepository = libraryEventsRepository;
+    }
+
+    public void processLibraryEvent(ConsumerRecord<Integer, String> consumerRecord) throws JsonProcessingException {
+
+        LibraryEvent libraryEvent = objectMapper.readValue(consumerRecord.value(), LibraryEvent.class);
+        log.info("Received Library Event: {}", libraryEvent.toString());
+
+        switch(libraryEvent.getLibraryEventType()){
+            case NEW:
+                save(libraryEvent);
+                break;
+            case UPDATE:
+                break;
+            default:
+                log.info("Invalid Library Event Type");
+        }
+
+    }
+
+    private void save(LibraryEvent libraryEvent) {
+        libraryEvent.getBook().setLibraryEvent(libraryEvent);
+        libraryEventsRepository.save(libraryEvent);
+        log.info("Saved Library Event: {}", libraryEvent.toString());
+    }
+}
